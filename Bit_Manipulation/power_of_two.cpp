3724c40eaf7a1622f6c0bc309f445e5c46d18ae4
AYUSH MISHRA <fightayush@gmail.com>
2026-06-30 12:45:39 +0530

    commit done

--- Bit_Manipulation/power_of_two.cpp
+++ Bit_Manipulation/power_of_two.cpp
@@ -5,7 +5,12 @@ using namespace std;
 int main(){
     int n;
     cin>>n;
-    if(n<=0) cout<<"false";
-    while(n%2==0) n/=2;
-    cout<<(n==1);
+    // if(n<=0) cout<<"false";
+    // while(n%2==0) n/=2;
+    // cout<<(n==1);
+
+
+    //XOR Method
+    if((n & (n-1))==0) cout<<true;
+    else cout<<false;
 }
